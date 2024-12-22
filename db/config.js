4f1926606ad49e7b6b27762613997a48a6f3c160
afifudin24 <afifrider507@gmail.com>
2024-12-22 20:52:43 +0700

    Perbaiki

--- db/config.js
+++ db/config.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '123',
-    database: 'siakaddaka'
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'siakaddaka',
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed: ' + err.stack);
-        return;
-    }
-    console.log('Connected to database.');
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database.');
 });
 
 module.exports = db;
